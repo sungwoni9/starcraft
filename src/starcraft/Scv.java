d5e488345f0abdc8f0eb4cf99649909df301e563
허성원 <hurdang99@naver.com>
2024-11-08 16:11:36 +0900

    Add : method repair()

--- src/starcraft/Scv.java
+++ src/starcraft/Scv.java
@@ -5,4 +5,22 @@ public class Scv extends Unit implements Repairable {
 		super("Scv", 55);
 	}
 
+	void repair(Repairable unit) {
+		if (unit instanceof Repairable) {
+			Unit target = (Unit) unit;
+
+			while (target.hp < target.MAX_HP) {
+				target.hp++;
+				String messege = String.format("%s 수리중[%d/%d]", target.name, target.hp, target.MAX_HP);
+				System.out.println(messege);
+
+				try {
+					Thread.sleep(50);
+				} catch (Exception e) {
+				}
+			}
+			System.out.println("수리완료 >>>");
+		}
+	}
+
 }
