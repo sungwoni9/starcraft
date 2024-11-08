d18ad1b06ca13e11ce499836f979aa4b2151e550
허성원 <hurdang99@naver.com>
2024-11-08 18:23:50 +0900

    Add : attack and repair

--- src/starcraft/Starcraft.java
+++ src/starcraft/Starcraft.java
@@ -1,5 +1,7 @@
 package starcraft;
 
+import interferses.Attackable;
+import interferses.Repairable;
 import units.Dropship;
 import units.Marine;
 import units.Scv;
@@ -7,6 +9,18 @@ import units.Tank;
 
 public class Starcraft {
 	public static void main(String[] args) {
+
+		Tank tank = new Tank();
+		Marine marine = new Marine();
+		Scv scv = new Scv();
+		Dropship ship = new Dropship();
+
+		Attackable.attack(marine, scv);
+		Attackable.attack(tank, ship);
+		Attackable.attack(scv, tank);
+
+		Repairable.repair(tank);
+		Repairable.repair(scv);
 
 	}
 
